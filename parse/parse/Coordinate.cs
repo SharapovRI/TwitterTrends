c5674fb7c1359bbbf59cd1371c72833fcde09c73
Ilya-w <79691568+Ilya-w@users.noreply.github.com>
2021-03-11 00:58:05 +0300

    Add files via upload

---
+++ parse/parse/Coordinate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterTrends.Models
+{
+    public class Coordinate
+    {
+        public Coordinate(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        private float x;
+        private float y;
+
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                if (value > 90 || value < -90)
+                    //throw new ArgumentOutOfRangeException("Неверное значение широты");
+                x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                if (value > 180 || value < -180)
+                    //throw new ArgumentOutOfRangeException("Неверное значение долготы");
+                y = value;
+            }
+        }
+    }
+}
